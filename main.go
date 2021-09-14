55de46dd349ef3325a06dc5fe5f1fae6dd049ba3
mesmerai <carmelocrivelli@gmail.com>
2021-09-14 15:54:07 +1000

    Closure on searchHandler and JSON data modeled

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/mesmerai/news-aggregator/news"
 )
 
-// visible to all main
+// Only needed with * Approach 1 * to make newsapi accessible to SearchHandler
 //var newsapi *news.Client
 
 // Loading HTML Template from file. Will will panic if error is not-nil.
@@ -33,10 +33,52 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 }
 
-/*
-	Receive a Pointer to news.Client and return an anonymous function that
-	satisfies the HandlerFunc
+/* *Approach 1 *
+1. newsapi var definition outside the main, so it is accessible everywhere in this file
+2. newsapi var assignment inside main
+3. searchHandler standard => mux.HandleFunc("/search", searchHandler)
 */
+/*
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	// Package url parses URLs and implements query escaping ==> http://localhost:8080/search?q=ciccio
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	// log the request
+	log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+
+	fmt.Println("Search Query: ", searchQuery)
+	fmt.Println("Page: ", page)
+
+	newsapi.FetchNews(searchQuery, page)
+
+}
+*/
+
+/* * Approach 2 *
+   * Closure / Functions declared inside of functions are special; they are closures. *
+
+	1. newsapi defined and assigned inside main
+	2. use a Closure to pass the newsapi inside searchHandler => mux.HandleFunc("/search", searchHandler(newsapi))
+	3. searchHandler function must be adjusted to include the Closure as per below
+
+	searchHandler receive a Pointer to news.Client and return an anonymous function that satisfies the HandlerFunc
+
+	NOTE: This is potentially a better solution since:
+	- it makes testing much easier
+	- limits the function's scope
+*/
+
 func searchHandler(newsapi *news.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Package url parses URLs and implements query escaping ==> http://localhost:8080/search?q=ciccio
@@ -56,11 +98,19 @@ func searchHandler(newsapi *news.Client) http.HandlerFunc {
 		// log the request
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 
-		fmt.Println("Search Query: ", searchQuery)
-		fmt.Println("Page: ", page)
+		//fmt.Println("Search Query: ", searchQuery)
+		//fmt.Println("Page: ", page)
 
-		newsapi.FetchNews(searchQuery, page)
+		results, err := newsapi.FetchNews(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+		// Printing the Results from the Search, the whole struct
+		// %v	the value in a default format
+		// when printing structs, the plus flag (%+v) adds field names
+		fmt.Printf("%+v", results)
 	}
 }
 
--- news/news.go
+++ news/news.go
@@ -1,19 +1,48 @@
 package news
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
+/* Article structs */
+type Article struct {
+	Source struct {
+		ID   interface{} `json:"id"`
+		Name string      `json:"name"`
+	} `json:"source"`
+	Author      interface{} `json:"author"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	URL         string      `json:"url"`
+	URLToImage  string      `json:"urlToImage"`
+	PublishedAt time.Time   `json:"publishedAt"`
+	Content     string      `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+/* Client is our struct for the News Client
+- http is a pointer to the httpClient itself that makes the web requests
+- key is the API key
+- pageSize is the number of results to return per page (max 100)
+*/
 type Client struct {
 	http     *http.Client
 	key      string
 	pageSize int
 }
 
+// NewClient function creates our Client used for requests
 func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
 	if pageSize > 100 {
 		pageSize = 100
@@ -22,26 +51,41 @@ func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
 	return &Client{httpClient, key, pageSize}
 }
 
-func (c *Client) FetchNews(query, page string) {
+// FetchNews func with 2 parameters (query and page) and return the Result struct
+//Notice that the search query is URL encoded through the QueryEscape() method.
+func (c *Client) FetchNews(query, page string) (*Results, error) {
 	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.pageSize, page, c.key)
 	resp, err := c.http.Get(endpoint)
 
 	// Handle error from the response
 	if err != nil {
 		log.Fatal("Error getting a response => ", err)
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
-	// Read the response / get 'body' in Bytes
+	// Response body is converted to a byte slice, if no errors
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
 		log.Fatal("Error reading the response => ", err)
+		return nil, err
 	}
 
-	// for now print Response Body and RetCode
-	fmt.Println(string(body))
-	fmt.Printf("Response Status: %s\n", resp.Status)
+	// this is for Printing Response Body and Ret Code
+	//fmt.Println(string(body))
+	//fmt.Printf("Response Status: %s\n", resp.Status)
+
+	// checking ret code, http.StatusOk is a const from http pkg
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	res := &Results{}
+	// func Unmarshal(data []byte, v interface{}) error
+	// Unmarshal parses the JSON-encoded data and stores the result in the value pointed to by v.
+	// If v is nil or not a pointer, Unmarshal returns an InvalidUnmarshalError.
+	return res, json.Unmarshal(body, res)
 
 }
